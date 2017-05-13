55c5eab8e8f1e87f8f697c52118514a0a7be85a2
Naman Aggarwal <naman.aggarwal@yahoo.com>
2017-05-13 22:12:59 +0800

    Added code to verify request from Facebook

--- app/botimpl/messengers.py
+++ app/botimpl/messengers.py
@@ -2,8 +2,15 @@ from app.bot import BaseMessenger
 
 class FacebookMessenger(BaseMessenger):
 
-    def __init__(self):
-        pass
+    def __init__(self,pageAccessToken=None,verifyToken=None):
+        self.pageAccessToken = pageAccessToken
+        self.verifyToken = verifyToken
+
+    def getVerifyToken(self):
+        return self.verifyToken
+
+    def getPageAccessToken(self):
+        return self.pageAccessToken        
 
     def send(self,receiverId,message):
         pass
--- app/views.py
+++ app/views.py
@@ -1,11 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort, request, current_app as app
 from model import User
-from botimpl import ChatBotController
+from botimpl import ChatBotController, FacebookMessenger
 
 pages = Blueprint('pages', __name__,template_folder='templates')
 
 @pages.route("/")
 def home():
-
     return render_template("home.html")
 
+
+@pages.route("/messenger",methods=['GET'])
+def facebookVerify():
+
+    #Create a messenger object
+    messenger = FacebookMessenger(app.config['FACEBOOK_PAGE_ACCESS_TOKEN'],app.config['FACEBOOK_VERIFY_TOKEN'])
+
+    #Get the request parameters from facebook
+    verify_token = request.args['hub.verify_token']
+    challenge = request.args['hub.challenge']
+
+    #Verify that the request came from facebook
+    if verify_token == messenger.getVerifyToken():
+        #Return the challenge
+        return challenge
+
+    else:
+        #Return Not found
+        abort(404)
+
+        
+
+
+    
